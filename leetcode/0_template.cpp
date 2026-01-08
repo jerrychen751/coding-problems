573c4a7bf674387080516dcc412bd9322ae77173
Jerry Chen <184151454+jerrychen751@users.noreply.github.com>
2026-01-08 16:53:55 -0600

    add types for common data structures

---
+++ leetcode/0_template.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+// Generic node definition
+class Node {
+public:
+    int val;
+    std::vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+// Binary tree node definition
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
