9ccd523fcc71e837b532c1def3087ac62c762f08
Jerry Chen <184151454+jerrychen751@users.noreply.github.com>
2026-08-15 13:51:37 -0500

    8.15.2026 - backtracking

---
+++ leetcode/47_permutations_2.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        '''
+        permutation is array of ints, return list of unique permutations
+        duplicates in nums
+
+        1, 2, 2
+
+        1 2 2
+        1 2 2
+        2 1 2
+        2 2 1
+        2 1 2
+        2 2 1
+
+        set of indices used
+        set of permutations generated
+        n*n! time and space
+
+        ordering becomes important to avoid duplicate permutations
+        used indices set
+        within the loop in recursive function, we want to skip forward indices to try as start of next branch until we find a different number
+
+        '''
+
+        res = []
+        nums.sort()
+        def backtrack(path: list[int], used: set[int]) -> None:
+            if len(used) == len(nums):
+                res.append(path.copy())
+                return
+
+            prev = None
+            for i in range(len(nums)):
+                if i in used:
+                    continue
+                if nums[i] == prev:
+                    continue
+
+                prev = nums[i]
+                path.append(nums[i])
+                used.add(i)
+                backtrack(path, used)
+                used.remove(i)
+                path.pop()
+
+        backtrack([], set())
+        return res
