a19bf7055ee6c0bb42228a8cd9aa02be1e295354
Jerry Chen <184151454+jerrychen751@users.noreply.github.com>
2026-08-18 15:28:46 -0500

    8.18.2026 - bit manipulation

---
+++ leetcode/136_single_number_1.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        bits = 0
+        for num in nums:
+            bits ^= num
+
+        return bits
